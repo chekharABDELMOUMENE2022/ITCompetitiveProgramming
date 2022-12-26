b356743252f3abe9e70689da8a223c303411b28b
chekharABDELMOUMENE2022 <121379538+chekharABDELMOUMENE2022@users.noreply.github.com>
2022-12-26 12:19:59 +0100

    Add files via upload

---
+++ ITCP/SecondDay/THE CHALLENGE/SUBMIT YOUR SOLUTION/problem 27.py
@@ -0,0 +1,9 @@
+x=int(input("entre le premier nombre"))
+y=int(input("entre le dexieme nombre"))
+def addition(x,y):
+    Z=x+y
+    if(Z>100):
+        print(False)
+    else:
+        print(True)
+addition(x,y)
